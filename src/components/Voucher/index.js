37a146541b4ea3d676c97444f57c753f1d97a08b
StavanIndiaBison <stavan.kulkarni@indiabison.com>
2024-11-12 10:07:56 +0530

    add delete voucher dialog

--- src/components/Voucher/index.js
+++ src/components/Voucher/index.js
@@ -6,6 +6,7 @@ import { Tab } from '@mui/base/Tab';
 import Box from "@mui/material/Box";
 import { Tabs } from '@mui/base/Tabs';
 import { Toast } from 'primereact/toast';
+import { Dialog } from 'primereact/dialog';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import 'primereact/resources/primereact.css';
@@ -16,6 +17,7 @@ import { InputIcon } from 'primereact/inputicon';
 import { InputText } from 'primereact/inputtext';
 import Typography from "@mui/material/Typography";
 import { DataTable } from 'primereact/datatable';
+import { PiWarningOctagonThin } from "react-icons/pi";
 import { ProductService } from '../../service/ProductService';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 
@@ -28,6 +30,7 @@ export default function Voucher() {
     //states
     const [products, setProducts] = useState(null);
     const [globalFilter, setGlobalFilter] = useState(null);
+    const [deleteProductDialog, setDeleteProductDialog] = useState(false);
 
     useEffect(() => {
         ProductService
@@ -65,7 +68,14 @@ export default function Voucher() {
             <React.Fragment>
                 <Button icon="pi pi-pencil" rounded outlined className="mr-2" tooltip='Edit voucher' tooltipOptions={{ position: 'bottom' }} />
                 <Button icon="pi pi-receipt" rounded outlined className="mr-2" tooltip='View voucher' tooltipOptions={{ position: 'bottom' }} />
-                <Button icon="pi pi-trash" rounded outlined severity="danger" tooltip='Delete voucher' tooltipOptions={{ position: 'bottom' }} />
+                <Button
+                    icon="pi pi-trash"
+                    rounded outlined
+                    severity="danger"
+                    tooltip='Delete voucher'
+                    tooltipOptions={{ position: 'bottom' }}
+                    onClick={() => setDeleteProductDialog(true)}
+                />
             </React.Fragment>
         );
     };
@@ -79,6 +89,34 @@ export default function Voucher() {
             </IconField>
         </div>
     );
+
+    const hideDeleteProductDialog = () => {
+        setDeleteProductDialog(false);
+    };
+
+    const deleteProductDialogFooter = (
+        <React.Fragment>
+            <Button label="Cancel" severity='secondary' outlined onClick={hideDeleteProductDialog} />
+            <Button label="OK" severity="danger" onClick={hideDeleteProductDialog} />
+        </React.Fragment>
+    );
+
+    const DeleteVoucherDialog = () => (
+        <Dialog
+            modal
+            style={{ width: '32rem' }}
+            visible={deleteProductDialog}
+            onHide={hideDeleteProductDialog}
+            footer={deleteProductDialogFooter}
+            breakpoints={{ '960px': '75vw', '641px': '90vw' }}
+        >
+            <div className="delete-dialog">
+                <PiWarningOctagonThin size={80} elevation={3} color='#CC0000' />
+                <h3>Are you sure?</h3>
+                <span>Once deleted, you will not be able to recover record!</span>
+            </div>
+        </Dialog>
+    )
 
     return (
         <Box
@@ -124,6 +162,7 @@ export default function Voucher() {
                                     <Column header="Action" body={actionBodyTemplate} style={{ minWidth: '12rem' }} />
                                 </DataTable>
                             </div>
+                            <DeleteVoucherDialog />
                         </div>
                     </TabPanel>
                     <TabPanel value={2}>
@@ -153,6 +192,7 @@ export default function Voucher() {
                                     <Column header="Action" body={actionBodyTemplate} style={{ minWidth: '12rem' }} />
                                 </DataTable>
                             </div>
+                            <DeleteVoucherDialog />
                         </div>
                     </TabPanel>
                 </Tabs>
