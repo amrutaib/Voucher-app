272f51a09ef5a463ee06d55b5a45070fd80c02e1
StavanIndiaBison <stavan.kulkarni@indiabison.com>
2024-11-14 17:05:34 +0530

    add destination add/view api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://c5da-110-226-177-100.ngrok-free.app';
 const api_routes = {
     add_user: '/save',
+    destination: '/destination',
     add_user_payment: '/payment'
 };
 
