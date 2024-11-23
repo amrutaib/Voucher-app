af7b8c574fe78922f5c36bd6c11ea9c90d301aaf
StavanIndiaBison <stavan.kulkarni@indiabison.com>
2024-11-23 11:51:00 +0530

    update navbar header

--- src/components/Navbar/NavBarHeader.js
+++ src/components/Navbar/NavBarHeader.js
@@ -1,80 +1,42 @@
-import { React, useState, useEffect } from "react";
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import IconButton from "@mui/material/IconButton";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
+import { Toolbar, Tooltip } from "@mui/material";
+import LogoutIcon from '@mui/icons-material/Logout';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const NavBarHeader = () => {
 
     const theme = useTheme();
-    const navigate = useNavigate();
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    //fetch email & password form localstorage
-    const getEmail = localStorage.getItem("emailData");
-    const getPassword = localStorage.getItem("passwordData");
-
-    useEffect(() => {
-        if (!getEmail && !getPassword) {
-            setIsLoggedIn(false);
-        } else {
-            setIsLoggedIn(true);
-        }
-    }, [getEmail, getPassword]);
-
-    const handleClick = () => {
-        setIsLoggedIn(false);
-        localStorage.clear();
-        navigate("/login");
-    };
 
     return (
         <Toolbar sx={{ position: 'absolute', right: '20px' }}>
             <Link to="/UserProfile">
-                <IconButton title="User">
+                <Tooltip title='User profile'>
                     <AccountCircleIcon
                         sx={{
-                            mr: '10px',
-                            fontSize: 40,
+                            mr: '20px',
+                            fontSize: 35,
                             color: "#e6f0ff",
-                            "&:hover": { bgcolor: "#e6e6e6" },
                             [theme.breakpoints.down("md")]: { fontSize: 30 },
                             [theme.breakpoints.down("sm")]: { fontSize: 25 }
                         }}
                     />
-                </IconButton>
+                </Tooltip>
             </Link>
-            {
-                isLoggedIn ? (
-                    <Button
+
+            <Link to="/login">
+                <Tooltip title='Log Out'>
+                    <LogoutIcon
                         sx={{
-                            color: "black",
-                            bgcolor: "#e6f0ff",
-                            "&:hover": { bgcolor: "#e6e6e6" },
-                            [theme.breakpoints.down("md")]: { fontSize: 10 },
-                            [theme.breakpoints.down("sm")]: { fontSize: 6 }
+                            fontSize: 30,
+                            color: "#e6f0ff",
+                            [theme.breakpoints.down("md")]: { fontSize: 30 },
+                            [theme.breakpoints.down("sm")]: { fontSize: 25 }
                         }}
-                        onClick={handleClick}
-                    >
-                        Logout
-                    </Button>
-                ) : (
-                    <Link to="/login">
-                        <Button
-                            sx={{
-                                color: "black",
-                                bgcolor: "#e6f0ff",
-                                "&:hover": { bgcolor: "#e6e6e6" }
-                            }}
-                        >
-                            Login
-                        </Button>
-                    </Link>
-                )
-            }
+                    />
+                </Tooltip>
+            </Link>
         </Toolbar>
     );
 }
